7006bf91452f880b21057d53c3403e1b2b978e30
Jorge Rea <jorge.lrs497@gmail.com>
2019-11-13 11:41:17 -0600

    cambios ligeros

--- core/view.php
+++ core/view.php
@@ -8,13 +8,7 @@ class View{
 
     function render($nombre){
         require_once "views/assets/head.php";
-
-        //Validando componentes
-        if(isset($_SESSION['usr_name']))        
-            require_once "views/components/user_navbar.php";
-        else
-            require_once "views/components/navbar.php";
-        
+        require_once "views/components/navbar.php";
         require_once "views/$nombre.php";
     }
 }
--- views/users/list.php
+++ views/users/list.php
@@ -1,7 +1,7 @@
 <body class="col sc pdgT100">
-    <div class="textBox fullW maxW500 pdg20">
+    <div class="textBox fullW maxW500 pdg20 col cc">
         <div class="mrg0 pdg0 pdg10 brn">Usuario</div>
-        <input type="text" class="ham-fblue fs20 ham-input brn boxshwblue" name="usrname" id="search">
+        <input type="text" class="ham-fblue fs20 ham-input brn boxshwblue" autocomplete="off" id="search">
     </div>
     <div class="container full">
         <div class="table-container">
