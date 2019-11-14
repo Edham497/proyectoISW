b077fa6fa53191f5154fa2465cd4fc6d07548718
Jorge Rea <jorge.lrs497@gmail.com>
2019-11-14 12:00:03 -0600

    reparando api

--- config/db.php
+++ config/db.php
@@ -1,5 +1,37 @@
-<?php
+<?php 
+
 class Database{
+
+    private $host;
+    private $db;
+    private $user;
+    private $password;
+    private $charset;
+
+    function __construct(){
+        $this->host = "localhost";
+        $this->db = "guarderia";
+        $this->user = "root";
+        $this->password = "";
+        $this->charset = "utf8mb4";
+    }
     
+    function conectarBD(){
+        try{
+            $connection = "mysql:host=$this->host; dbname=$this->db; charset=$this->charset;";
+            $options = [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES => false
+            ];
+            return new PDO($connection, $this->user, $this->password, $options);   
+        }
+        catch(PDOException $e){
+            print_r('Error connection' . $e->getMessage());
+        }
+    }
+
 }
+
 ?>
