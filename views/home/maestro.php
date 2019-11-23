d859f061733d67ccad5e302f323dbcf28d8f2ab1
Jorge Rea <jorge.lrs497@gmail.com>
2019-11-23 15:00:54 -0600

    limpiando rama

--- views/home/maestro.php
+++
@@ -1,5 +0,0 @@
-<body class="material-dark fwhite">
-    <div class="main col cc">
-        <div class="ham-title">Maestro</div>
-    </div>
-</body>
--- views/login/pass_recovery.php
+++
@@ -1,3 +0,0 @@
-<body class="of-hidden col cc ham-blue">
-    <div class="display4 tac pdg20">Recuperar Contraseña</div>
-</body>
