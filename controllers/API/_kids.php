8ecb5788d1162de999d702eb4082c05ea4431a1d
Jorge Rea <jorge.lrs497@gmail.com>
2019-11-26 23:20:54 -0600

    listar niños en tutor

---
+++ controllers/API/_kids.php
@@ -0,0 +1,91 @@
+<?php
+require_once 'core/db.php';
+class _kids{
+    function __construct(){
+        $this->db = new Database();
+        // Obtiene la informacion de la n
+        $this->json = file_get_contents('php://input');
+        // Convierte el JSON a un Objeto de PHP
+        $this->data = json_decode($this->json);
+    }
+    //LISTADO DE TODOS LOS NIÑOS
+    function listKids(){
+        $conn = $this->db->getConn();
+        $query = "SELECT nomNiño, apPNiño, apMNiño,fecNNiño,grupofk,imgNiño FROM Niño WHERE activo =  true;";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        $stmt = $stmt->fetchAll(PDO::FETCH_OBJ);
+        echo json_encode($stmt);
+    }
+    //LISTAR NIÑOS POR TUTOR
+    function listKidsTut($id){
+        $conn = $this->db->getConn();
+        $query = "SELECT * FROM Niño WHERE idNiño in (SELECT idNiñofk FROM TutAut_Niño where idTutor = :id);";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":id",$this->data->id);
+        $stmt->execute();
+        $stmt = $stmt->fetchAll(PDO::FETCH_OBJ);
+        echo json_encode($stmt);
+    }
+    //INSERTAR NIÑOS
+    function insertKid($nom,$apP,$apM,$fec,$grupo){
+        $conn = $this->db->getConn();
+        $query = "INSERT INTO Niño (nomNiño, apPNiño, apMNiño, fecNNiño, grupofk, activo) VALUES ('$nom','$apP','$apM','$fec','$grupo',1)";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        json_encode("DATO INSETADO CORRECTAMENTE");
+    }
+    //ACTUALIZAR INFO. DE NIÑOS
+    function updateKid($id,$nom,$apP,$apM,$fec,$grupo){
+        $conn = $this->db->getConn();
+        $query = "UPDATE Niño SET nomNiño = '$nom', apPNiño = '$apP', apMNiño = '$apM', fecNNiño = '$fec', grupo = '$grupo' WHERE idNiño = $id;";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        json_encode("DATO ACTUALIZADO CORRECTAMENTE");
+    }
+    //DAR DE BAJA A UN NIÑO
+    function deleteKid($id){
+        $conn = $this->db->getConn();
+        $query = "UPDATE Niño SET activo = false WHERE idNiño = $id;";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        json_encode("DADO DE BAJA CORRECTAMENTE");
+    }
+    //UNIR NIÑOS CON AUTORIZADOS Y TUTOR
+    function kidTutAut($idTut,$idNiño){
+        $conn = $this->db->getConn();
+        $query = "INSERT INTO TutAut_Niño (idTutor, idNiñofk) VALUES ($idTut,$idNiño);";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        json_encode("DATO INSETADO CORRECTAMENTE");
+    }
+    //REGISTRAR ENTRADA
+    function entryKid($id){
+        $conn = $this->db->getConn();
+        $query = "INSERT INTO Asistencia (idNiñofk,fecEntrada) VALUES ($id,NOW());";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+        json_encode("ENTRADA CONFIRMADA");
+    }
+    //REGISTRAR SALIDA
+    function exitKid($idPR,$idTutAut){
+        $conn = $this->db->getConn();
+        $query = "UPDATE Asistencia SET fecSalida=NOW(), idPR = '$idPR', idTutAut = $idTutAut;";
+        $stmt = $conn->prepare($query);
+        $stmt->execute();
+    }
+    
+    /*LOS METODOS SE LA SUDABA, AGREGE UN ATRIBUTO LLAMADA "method" AL JSON PARA EL METODO
+    switch($data->method){
+        case "list":listKids();break;
+            case "listByTut":listKidsTut($data->id);break;
+            case "insert": insertKid($data->nom,$data->apP,$data->apM,$data->fec,$data->grupo);break;
+            case "update": updateKid($data->id,$data->nom,$data->apP,$data->apM,$data->fec,$data->grupo);break;
+            case "delete":deleteKid($data->id);break;
+            case "kidTutAut":kidTutAut($data->idTut,$data->idNiño);break;
+            case "entry": entryKid($data->id);break;
+            case "exit":exitKid($data->idPR,$data->idTutAut);break;
+        }*/
+}
+
+?>
--- controllers/API/_users.php
+++ controllers/API/_users.php
@@ -10,7 +10,7 @@ class _users{
         try{
             $stmt = $this->db->getConn();
             
-            $query = "SELECT idUsuario, nomUsuario, apPUsuario, apMUsuario, fecNUsuario, email, direccion, telefono FROM usuario";
+            $query = "SELECT idUsuario, nomUsuario, apPUsuario, apMUsuario, fecNUsuario, email, direccion, telefono FROM usuario where idUsuario > 1 and activo = true";
             // $query = "SELECT * FROM Maestro_Niño join Niño on idNiño = idNiñofk";
             //$query = "SELECT *,TIMESTAMPDIFF(HOUR,fechaEntrada,fechaSalida),TIMESTAMPDIFF(MINUTES,fechaEntrada,fechaSalida) FROM Maestro_Niño";
             // echo $query;
@@ -33,7 +33,7 @@ class _users{
         try{
             $stmt = $this->db->getConn();
             
-            $query = "SELECT idUsuario, nomUsuario, apPUsuario, apMUsuario, fecNUsuario, email, direccion, telefono FROM usuario where idUsuario = $id";
+            $query = "SELECT idUsuario, nomUsuario, apPUsuario, apMUsuario, fecNUsuario, email, direccion, telefono FROM usuario where idUsuario = $id AND activo = true";
             $stmt = $stmt->query($query);
             $stmt = $stmt->fetch(PDO::FETCH_OBJ);
             if($stmt){
@@ -49,17 +49,51 @@ class _users{
         }
 
     }
-    // idUsuario,
-    // nomUsuario,
-    // apPUsuario,
-    // apMUsuario,
-    // fecNUsuario date,
-    // email,
-    // direccion,
-    // telefono,
-    // rol,
-    // imgUsuario varchar(255),
-    // activo boolean DEFAULT FALSE
+    function insertUser($nom,$apP,$apM,$fec,$email,$pass,$dir,$tel,$rol){
+            $db = new Database();
+            $conn = $db->getConn();
+            $passR = password_hash($pass,PASSWORD_DEFAULT);
+            $query = "INSERT INTO Usuario ( nomUsuario, apPUsuario, apMUsuario, fecNUsuario, email, pass, direccion, telefono, rol, activo) VALUES ('$nom','$apP','$apM','$fec','$email','$passR','$dir','$tel',$rol,1);";
+            $stmt = $conn->prepare($query);
+            $stmt->execute();
+            echo json_encode("DATO INSETADO CORRECTAMENTE");
+        }
+        //INSERTAR AUTORIZADOS
+        function insertUserAut($nom,$apP,$apM,$fec,$dir,$tel){
+            $db = new Database();
+            $conn = $db->getConn();
+            $query = "INSERT INTO Usuario ( nomUsuario, apPUsuario, apMUsuario, fecNUsuario, direccion, telefono, rol, activo) VALUES ('$nom','$apP','$apM','$fec','$dir','$tel',6,1);";
+            $stmt = $conn->prepare($query);
+            $stmt->execute();
+            echo json_encode("DATO INSETADO CORRECTAMENTE");
+        }
+        //ACTUALIZAR USUARIO
+        function updateUser($id,$nom,$apP,$apM,$fec,$email,$pass,$dir,$tel,$rol){
+            $db = new Database();
+            $conn = $db->getConn();
+            $passR = password_hash($pass,PASSWORD_DEFAULT);
+            $query = "UPDATE Usuario SET nomUsuario = '$nom', apPUsuario = '$apP', apMUsuario = '$apM', fecNUsuario = '$fec', email = '$email', pass = '$passR',direccion = '$dir', telefono = '$tel', rol = $rol WHERE idUsuario = $id;";
+            $stmt = $conn->prepare($query);
+            $stmt->execute();
+            echo json_encode("DATO ACTUALIZADO CORRECTAMENTE");
+        }
+        //DAR DE BAJA USUARIOS
+        function deleteUser($id){
+            $db = new Database();
+            $conn = $db->getConn();
+            $query = "UPDATE Usuario SET activo = false WHERE idUsuario = $id;";
+            $stmt = $conn->prepare($query);
+            $stmt->execute();
+            echo json_encode("DADO DE BAJA CORRECTAMENTE");
+        }
 }
+/*LOS METODOS SE LA SUDABA, AGREGE UN ATRIBUTO LLAMADA "method" AL JSON PARA EL METODO
+switch($data->method){
+    case "list":listUsers();break;
+    case "insert": insertUser($data->nom,$data->apP,$data->apM,$data->fec,$data->email,$data->pass,$data->dir,$data->tel,$data->rol);break;
+    case "insertAut": insertUserAut($data->nom,$data->apP,$data->apM,$data->fec,$data->dir,$data->tel);break;
+    case "update": updateUser($data->id,$data->nom,$data->apP,$data->apM,$data->fec,$data->email,$data->pass,$data->dir,$data->tel,$data->rol);break;
+    case "delete":deleteUser($data->id);break;
+}*/
 
 ?>
