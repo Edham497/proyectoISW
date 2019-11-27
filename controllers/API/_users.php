a542754d87ccc80bb4c101a1c12ebb09f5846546
Naer <hector.naer@gmail.com>
2019-11-26 23:44:55 -0600

    mario es puto

--- controllers/API/_users.php
+++ controllers/API/_users.php
@@ -4,6 +4,8 @@ require_once 'core/db.php';
 class _users{
     function __construct(){
         $this->db = new Database();
+        $this->json = file_get_contents('php://input');
+        $this->data = json_decode($this->json);
     }
 
     function list(){
@@ -48,6 +50,44 @@ class _users{
             ]]);
         }
 
+    }
+
+    //INSERTAR AUTORIZADOS 
+    function insertUser(){ 
+        $db = new Database(); 
+        $conn = $db->getConn(); 
+        $passR = password_hash($this->data->pass,PASSWORD_DEFAULT); 
+        $query = "INSERT INTO Usuario ( nomUsuario, apPUsuario, apMUsuario, fecNUsuario, email, pass, direccion, telefono, rol, activo) VALUES ('".$this->data->nom."','".$this->data->apP."','".$this->data->apM."','".$this->data->fec."','".$this->data->email."','$passR','".$this->data->dir."','".$this->data->tel."',".$this->data->rol.",1);"; 
+        $stmt = $conn->prepare($query); 
+        $stmt->execute(); 
+        echo json_encode("DATO INSETADO CORRECTAMENTE"); 
+    } 
+
+    function insertUserAut(){ 
+        $db = new Database(); 
+        $conn = $db->getConn(); 
+        $query = "INSERT INTO Usuario ( nomUsuario, apPUsuario, apMUsuario, fecNUsuario, direccion, telefono, rol, activo) VALUES ('".$this->data->nom."','".$this->data->apP."','".$this->data->apM."','".$this->data->fec."','".$this->data->dir."','".$this->data->tel."',6,1);"; 
+        $stmt = $conn->prepare($query); 
+        $stmt->execute(); 
+        echo json_encode("DATO INSETADO CORRECTAMENTE"); 
+    } 
+    //ACTUALIZAR USUARIO 
+    function updateUser(){ 
+        $db = new Database(); 
+        $conn = $db->getConn(); 
+        $passR = password_hash($this->data->pass,PASSWORD_DEFAULT); 
+        $query = "UPDATE Usuario SET nomUsuario = '".$this->data->nom."', apPUsuario = '".$this->data->apP."', apMUsuario = '".$this->data->apM."', fecNUsuario = '".$this->data->fec."', email = '".$this->data->email."', pass = '$passR',direccion = '".$this->data->dir."', telefono = '".$this->data->tel."' , rol = ".$this->data->rol." WHERE idUsuario = ".$this->data->id.";"; 
+        $stmt = $conn->prepare($query); 
+        $stmt->execute(); 
+        echo json_encode("DATO ACTUALIZADO CORRECTAMENTE"); 
+    } 
+    function deleteUser(){ 
+        $db = new Database(); 
+        $conn = $db->getConn(); 
+        $query = "UPDATE Usuario SET activo = false WHERE idUsuario = ".$this->data->id.";"; 
+        $stmt = $conn->prepare($query); 
+        $stmt->execute(); 
+        echo json_encode("DADO DE BAJA CORRECTAMENTE"); 
     }
     // idUsuario,
     // nomUsuario,
