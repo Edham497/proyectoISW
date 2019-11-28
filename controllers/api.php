c947c975af2c3442dcb466f555db059e9c12b869
Jorge Rea <jorge.lrs497@gmail.com>
2019-11-27 21:09:57 -0600

    Bloqueos no autorizados en la API

--- controllers/api.php
+++ controllers/api.php
@@ -4,19 +4,23 @@ require_once 'core/_err.php';
 
 class API{
     function __construct($URL){
-        if(isset($URL[1])){
-            $nameAPI = "_$URL[1]";
-            if(file_exists(self::getApiPath($nameAPI))){
-                require_once self::getApiPath($nameAPI);
-                $classAPI = new $nameAPI;
-    
-                //si hayun metodoo que exista lo llamamos
-                if(isset($URL[2]) && method_exists($classAPI, $URL[2]))
-                    //al mismo tiempo, si hay un argumento lo llamamos
-                    $classAPI->{$URL[2]}(isset($URL[3])?$URL[3]:'');
+        if(Router::isLoged()){
+            if(isset($URL[1])){
+                $nameAPI = "_$URL[1]";
+                if(file_exists(self::getApiPath($nameAPI))){
+                    require_once self::getApiPath($nameAPI);
+                    $classAPI = new $nameAPI;
+                    //si hayun metodoo que exista lo llamamos
+                    if(isset($URL[2]) && method_exists($classAPI, $URL[2])){
+                        //al mismo tiempo, si hay un argumento lo llamamos
+                        $classAPI->{$URL[2]}(isset($URL[3])?$URL[3]:'');
+                    }
+                }
+                else new _error(400);
             }
+            else new _error(400);
         }
-        else new _error(400);
+        else new _error(403);
     }
     
     private static function getApiPath($nameAPI){
